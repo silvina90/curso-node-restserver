0f1d4149424b6397c494eec121252586b21a6825
Silvina Cardetti <cardettisilvina@gmail.com>
2022-04-12 16:43:49 -0300

    google singin

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,9 @@ const { response } = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
+const { json } = require('express/lib/response');
+const usuario = require('../models/usuario');
 
 
 const login = async (req, res = response) => {
@@ -29,7 +32,7 @@ const login = async (req, res = response) => {
             })
         }
         //generar JWT
-        const token=await generarJWT(usuario.id);
+        const token = await generarJWT(usuario.id);
         res.json({
             usuario,
             token
@@ -42,6 +45,51 @@ const login = async (req, res = response) => {
         });
     }
 
-}
 
-module.exports = { login }
+}
+const googleSingIn = async (req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            //creo usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            }
+            usuario = new Usuario(data)
+            await usuario.save();
+        }
+        //si el usurio en DB 
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'hable con el administrador, usuario bloqueado'
+            })
+        }
+        const token = await generarJWT(usuario.id);
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'Token de Google no valido'
+        })
+    }
+
+
+
+
+
+}
+module.exports = {
+    login,
+    googleSingIn
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlwares/validar-campos');
 
 const router = Router();
@@ -11,5 +11,9 @@ router.post('/login',[
     check('password', 'el password es obligatorio').not().isEmpty(),
     validarCampos], login);
 
+    router.post('/google',[
+        check('id_token', 'id_token es necesaro').not().isEmpty(),
+        validarCampos], googleSingIn);
+    
 
 module.exports=router;
